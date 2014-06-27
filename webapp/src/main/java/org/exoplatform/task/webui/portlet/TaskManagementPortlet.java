f0a65047185cb33e58f4114af41b78ce6ffa7043
Pham Duy Dong <dongpd@exoplatform.com>
2014-06-27 15:14:57 +0700

    TaskManagementPortlet with dumb data

--- webapp/src/main/java/org/exoplatform/task/webui/portlet/TaskManagementPortlet.java
+++ webapp/src/main/java/org/exoplatform/task/webui/portlet/TaskManagementPortlet.java
@@ -1,5 +1,7 @@
 package org.exoplatform.task.webui.portlet;
 
+import org.exoplatform.codefest.services.api.TaskManager;
+import org.exoplatform.codefest.services.utils.CoreUtils;
 import org.exoplatform.container.PortalContainer;
 import org.exoplatform.webui.application.WebuiApplication;
 import org.exoplatform.webui.application.WebuiRequestContext;
@@ -11,6 +13,8 @@ import org.exoplatform.webui.event.Event;
 import org.exoplatform.webui.event.EventListener;
 import org.exoplatform.codefest.services.model.Task;
 
+import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 @ComponentConfig(
@@ -29,7 +33,50 @@ public class TaskManagementPortlet extends UIPortletApplication {
   }
 
   public List<Task> getTaskList(String projectId) {
-    return null;
+    List<Task> taskList = new ArrayList<Task>();
+    Task tempTask = new Task();
+    tempTask.setName("Can't add or edit \"Illustrated Web Content\" with filling field \"JS Data\"");
+    tempTask.setDueDate(new Date());
+    tempTask.setPriority("Minor");
+    tempTask.setStatus("In progress");
+    taskList.add(tempTask);
+
+    tempTask = new Task();
+    tempTask.setName("[IE] Failed upload and throw RepositoryException when upload a zip file");
+    tempTask.setDueDate(new Date());
+    tempTask.setPriority("Low");
+    tempTask.setStatus("Open");
+    taskList.add(tempTask);
+
+    tempTask = new Task();
+    tempTask.setName("Unplanned stuff for sprint PLF-2014-S26");
+    tempTask.setDueDate(new Date());
+    tempTask.setPriority("High");
+    tempTask.setStatus("Closed");
+    taskList.add(tempTask);
+
+    tempTask = new Task();
+    tempTask.setName("Impossible to open uploaded file in Chinese from Upload Status box");
+    tempTask.setDueDate(new Date());
+    tempTask.setPriority("High");
+    tempTask.setStatus("In progress");
+    taskList.add(tempTask);
+
+    tempTask = new Task();
+    tempTask.setName("Limit size of upload file if they are uploaded by webdav client tools.");
+    tempTask.setDueDate(new Date());
+    tempTask.setPriority("Low");
+    tempTask.setStatus("Open");
+    taskList.add(tempTask);
+
+    tempTask = new Task();
+    tempTask.setName("[Weemo-extension] Not redirected to space after created and space menu disappears when updating navigation of space");
+    tempTask.setDueDate(new Date());
+    tempTask.setPriority("High");
+    tempTask.setStatus("Closed");
+    taskList.add(tempTask);
+    return taskList;
+   // return CoreUtils.getService(TaskManager.class).getAllTaskInProject(projectId);
   }
 
   public void processRender(WebuiApplication app, WebuiRequestContext context) throws Exception {
