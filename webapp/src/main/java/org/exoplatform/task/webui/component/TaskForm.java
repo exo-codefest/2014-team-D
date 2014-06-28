20ee2d996613f889797a1c6926ba34f2668c8a7a
Pham Duy Dong <dongpd@exoplatform.com>
2014-06-28 22:18:30 +0100

    TaskForm - add

--- webapp/src/main/java/org/exoplatform/task/webui/component/TaskForm.java
+++ webapp/src/main/java/org/exoplatform/task/webui/component/TaskForm.java
@@ -1,5 +1,11 @@
 package org.exoplatform.task.webui.component;
 
+import org.exoplatform.codefest.services.api.ProjectManager;
+import org.exoplatform.codefest.services.api.TaskManager;
+import org.exoplatform.codefest.services.model.Project;
+import org.exoplatform.codefest.services.model.Task;
+import org.exoplatform.codefest.services.utils.CoreUtils;
+import org.exoplatform.task.webui.portlet.TaskManagementPortlet;
 import org.exoplatform.webui.config.annotation.ComponentConfig;
 import org.exoplatform.webui.config.annotation.EventConfig;
 import org.exoplatform.webui.core.UIPopupWindow;
@@ -26,34 +32,86 @@ import java.util.List;
 )
 public class TaskForm extends UIForm {
 
-  public TaskForm() {
+  private static final String TASK_PROJECT_ID = "taskProjectId";
+  private static final String TASK_PROJECT_SUMMARY = "taskProjectSummary";
+  private static final String TASK_PROJECT_DESCRIPTION = "taskProjectDescription";
+  private static final String TASK_PROJECT_PRIORITY = "taskProjectPriority";
+  private static final String TASK_PROJECT_STATUS = "taskProjectStatus";
+  private static final String TASK_PROJECT_REPORTER = "taskReporterReporter";
+  private static final String TASK_PROJECT_ASSIGNEE = "taskProjectAssignee";
+  private static final String TASK_PROJECT_ESTIMATE_TIME = "taskProjectEstimateTime";
+  private static final String TASK_DUE_DATE = "taskDueDate";
+
+  public TaskForm() throws Exception {
+
+
+    // ProjectId
     List<SelectItemOption<String>> projectOptions = new ArrayList<SelectItemOption<String>>();
-    projectOptions.add(new SelectItemOption<String>("Uncategoried", "Uncategoried"));
-    projectOptions.add(new SelectItemOption<String>("HR", "hr"));
-    projectOptions.add(new SelectItemOption<String>("ECMS", "ecms"));
-    UIFormSelectBox taskProjectUIFormSelectBox = new UIFormSelectBox("taskProject", "taskProject", projectOptions);
-    UIFormStringInput taskNameUIFormStringInput = new UIFormStringInput("taskName", "taskName", null);
-    UIFormDateTimeInput taskDueDateUIFormDateTimeInput = new UIFormDateTimeInput("taskDueDate", "taskDueDate", null);
+    List<Project> allProjects = CoreUtils.getService(ProjectManager.class).getProjects();
+    for (Project project : allProjects) {
+      projectOptions.add(new SelectItemOption<String>(project.getName(), project.getName()));
+    }
+    UIFormSelectBox taskProjectIdUIFormSelectBox = new UIFormSelectBox(TASK_PROJECT_ID, TASK_PROJECT_ID, projectOptions);
+
+    // Summary
+    UIFormStringInput taskProjectSummaryUIFormStringInput = new UIFormStringInput(TASK_PROJECT_SUMMARY, TASK_PROJECT_SUMMARY, null);
+
+    // Description
+    //UIFormTextAreaInput taskProjectDescriptionUIFormTextAreaInput = new UIFormTextAreaInput(TASK_PROJECT_DESCRIPTION, TASK_PROJECT_DESCRIPTION, null);
+
+    // Priority
     List<SelectItemOption<String>> priorityOptions = new ArrayList<SelectItemOption<String>>();
     priorityOptions.add(new SelectItemOption<String>("High", "high"));
     priorityOptions.add(new SelectItemOption<String>("Medium", "medium"));
-    priorityOptions.add(new SelectItemOption<String>("Minor", "minor"));
-    UIFormSelectBox taskPriorityUIFormSelectBox = new UIFormSelectBox("taskPriority", "taskPriority", priorityOptions);
-    List<SelectItemOption<String>> statusOptions = new ArrayList<SelectItemOption<String>>();
-    statusOptions.add(new SelectItemOption<String>("Open", "open"));
-    statusOptions.add(new SelectItemOption<String>("In progress", "inprogress"));
-    statusOptions.add(new SelectItemOption<String>("Closed", "closed"));
-    UIFormSelectBox taskStatusUIFormSelectBox = new UIFormSelectBox("taskStatus", "taskStatus", statusOptions);
-    addUIFormInput(taskProjectUIFormSelectBox);
-    addUIFormInput(taskNameUIFormStringInput);
-    addUIFormInput(taskDueDateUIFormDateTimeInput);
-    addUIFormInput(taskPriorityUIFormSelectBox);
-    addUIFormInput(taskStatusUIFormSelectBox);
-    setActions(new String[]{"Save","Cancel"})  ;
+    priorityOptions.add(new SelectItemOption<String>("Low", "low"));
+    UIFormSelectBox taskProjectPriorityUIFormSelectBox = new UIFormSelectBox(TASK_PROJECT_PRIORITY, TASK_PROJECT_PRIORITY, priorityOptions);
+
+    // Status
+//    List<SelectItemOption<String>> statusOptions = new ArrayList<SelectItemOption<String>>();
+//    statusOptions.add(new SelectItemOption<String>(TaskState.STATE_OPEN , TaskState.STATE_OPEN));
+//    statusOptions.add(new SelectItemOption<String>(TaskState.STATE_REOPENED , TaskState.STATE_REOPENED));
+//    statusOptions.add(new SelectItemOption<String>(TaskState.STATE_INPREGRESS , TaskState.STATE_INPREGRESS));
+//    statusOptions.add(new SelectItemOption<String>(TaskState.STATE_REOPENED , TaskState.STATE_RESOLVED));
+//    statusOptions.add(new SelectItemOption<String>(TaskState.STATE_CLOSED , TaskState.STATE_CLOSED));
+//    statusOptions.add(new SelectItemOption<String>(TaskState.STATE_AWAITING , TaskState.STATE_AWAITING));
+//    UIFormSelectBox taskProjectStatusUIFormSelectBox = new UIFormSelectBox(TASK_PROJECT_STATUS, TASK_PROJECT_STATUS, statusOptions);
+
+    // Assignee
+    UIFormStringInput taskProjectAssigneeUIFormStringInput = new UIFormStringInput(TASK_PROJECT_ASSIGNEE, TASK_PROJECT_ASSIGNEE, null);
+
+    // Estimate time
+    //UIFormStringInput taskProjectEstimateTimeUIFormStringInput = new UIFormStringInput(TASK_PROJECT_ESTIMATE_TIME, TASK_PROJECT_ESTIMATE_TIME, null);
+
+    // Due Date
+    UIFormDateTimeInput taskProjectDueDateUIFormDateTimeInput = new UIFormDateTimeInput(TASK_DUE_DATE, TASK_DUE_DATE, null);
+
+    addUIFormInput(taskProjectIdUIFormSelectBox);
+    addUIFormInput(taskProjectSummaryUIFormStringInput);
+    //addUIFormInput(taskProjectDescriptionUIFormTextAreaInput);
+    addUIFormInput(taskProjectPriorityUIFormSelectBox);
+    //addUIFormInput(taskProjectStatusUIFormSelectBox);
+    addUIFormInput(taskProjectAssigneeUIFormStringInput);
+    //addUIFormInput(taskProjectEstimateTimeUIFormStringInput);
+    addUIFormInput(taskProjectDueDateUIFormDateTimeInput);
+
+    setActions(new String[]{"Save", "Cancel"});
   }
 
   static public class SaveActionListener extends EventListener<TaskForm> {
     public void execute(Event<TaskForm> event) throws Exception {
+      TaskForm taskForm = event.getSource();
+      String projectId = taskForm.getUIStringInput(TASK_PROJECT_ID).getValue();
+      Task task = new Task();
+      task.setProjectId(projectId);
+      task.setSummary(taskForm.getUIStringInput(TASK_PROJECT_SUMMARY).getValue());
+      task.setDueDate(taskForm.getUIFormDateTimeInput(TASK_DUE_DATE).getCalendar());
+      //task.setStatus(taskForm.getUIFormSelectBox(TASK_PROJECT_STATUS).getValue());
+      task.setAssignee(taskForm.getUIStringInput(TASK_PROJECT_ASSIGNEE).getValue());
+      task.setPriority(taskForm.getUIFormSelectBox(TASK_PROJECT_PRIORITY).getValue());
+      task.setReporter(event.getRequestContext().getRemoteUser());
+      CoreUtils.getService(TaskManager.class).createTask(projectId, task);
+      event.getRequestContext().addUIComponentToUpdateByAjax(taskForm.getAncestorOfType(TaskManagementPortlet.class));
+      taskForm.getAncestorOfType(UIPopupWindow.class).setRendered(false);
     }
   }
 
